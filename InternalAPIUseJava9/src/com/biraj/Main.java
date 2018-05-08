7942cebb41aa29ef0a41a94f0da18498f933f87f
biraj <biraj.dhungel@haw-hamburg.de>
2018-05-08 10:58:45 +0200

    initial project + internal api use example

---
+++ InternalAPIUseJava9/src/com/biraj/Main.java
@@ -0,0 +1,10 @@
+package com.biraj;
+
+public class Main {
+	
+	public static void main(String []args)throws Exception{
+		sun.security.x509.X500Name name = new sun.security.x509.X500Name("cn=user");
+
+	}
+
+}
