00ffd8189178562b5d6420c6711c588598e0e8df
biraj <biraj.dhungel@haw-hamburg.de>
2018-05-09 12:51:43 +0200

    java 9 with monolithic module

---
+++ MonolithicAppointmentBookingJava9/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+/**
+ * @author biraj
+ *
+ */
+module appointmentBooking {
+	requires hibernate.jpa;
+	requires jackson.annotations;
+	requires spring.beans;
+	requires spring.boot;
+	requires spring.boot.autoconfigure;
+	requires spring.context;
+	requires spring.core;
+	requires spring.data.commons;
+	requires spring.data.jpa;
+	requires spring.web;
+}
